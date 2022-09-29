0c69e51cf291fa07a10ed59a9f6c5a07c0b1f5f1
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-09-29 13:34:59 +0200

    Improving group settings

--- pages/dashboard/ownedgroup/[groupId].js
+++ pages/dashboard/ownedgroup/[groupId].js
@@ -38,11 +38,11 @@ export default function OwnedGroup() {
                 </Link>
                 <h1 className={styles.title}>{groupName}</h1>
             </div>
-            <div className='line'>
-            <p>Invite link</p> <a className={styles.inviteLink} href={link ? link : '#'} target="_blank" rel="noreferrer">{link ? link : "No invitation link found"}</a>
-            <svg onClick={() => {navigator.clipboard.writeText(link);popup("Success", `Copied in the clipboard`, "default", {icon: drop})}} className={styles.linkIcon} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
-            </svg>
+            <div className="line">
+                <p>Invite link</p> <a className={styles.inviteLink} href={link ? link : '#'} target="_blank" rel="noreferrer">{link ? link : "No invitation link found"}</a>
+                <svg onClick={() => {navigator.clipboard.writeText(link);popup("Success", `Copied in the clipboard`, "default", {icon: drop})}} className={styles.linkIcon} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+                </svg>
             </div>
             <button onClick={() => fetch(`${serverIp}generate_interserv_group_link`, { method: 'POST', body : `{ "token": "${getCookie('token')}", "groupId": ${groupId}, "guildId":"${guildId}" }`}).then(res => res.json()).then(datas => {
                 if (datas.result) {
