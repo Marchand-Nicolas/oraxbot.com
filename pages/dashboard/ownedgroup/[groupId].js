14765ee0af4812f49994ff0a63549030a02c1f59
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-10-22 16:24:54 +0200

    Showing linked channels in a group, so they can be removed

--- pages/dashboard/ownedgroup/[groupId].js
+++ pages/dashboard/ownedgroup/[groupId].js
@@ -14,11 +14,13 @@ export default function OwnedGroup() {
     const router = useRouter()
     const { groupId } = router.query
     const [link, setLink] = useState('')
+    const [channels, setChannels] = useState([])
     useEffect(() => {
         if (groupId) {
             fetch(`${serverIp}get_admin_group_datas`, { method: 'POST', body : `{ "token": "${getCookie('token')}", "groupId": ${groupId}, "guildId":"${guildId}" }` }).then(res => res.json()).then(datas => {
                 if (datas.result) {
                     setLink("https://oraxbot.com/join/" + datas.link)
+                    setChannels(datas.channels)
                 }
             })
         }
@@ -49,6 +51,31 @@ export default function OwnedGroup() {
                     setLink( "https://oraxbot.com/join/" + datas.link)
                 }
             })} className={styles.regenerateInviteLink}>Regenerate invite link</button>
+            <br></br>
+            {channels.length ?
+            <section>
+                <h2>Linked channels</h2>
+                <div className={styles.channelList}>
+                    {channels.map((channel, index) => <div key={"channel_" + index} className={styles.channelContainer}>
+                        <p>{channel.guildName}</p>
+                        <svg className={styles.channelIcon} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
+                        </svg>
+                        <p>{channel.name}</p>
+                        <svg onClick={() => {
+                            fetch(`${serverIp}unlink_channel`, { method: 'POST', body : `{ "token": "${getCookie('token')}", "groupId":${groupId}, "channelId": "${channel.id}", "guildId":"${guildId}" }`}).then(res => res.json()).then(datas => {
+                                if (datas.result) {
+                                    setChannels(channels.filter(c => c.id != channel.id))
+                                }
+                            })
+                        }} className={styles.removeChannelIcon} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+                    </div>)}
+                </div>
+            </section>
+            :
+            <section className={dashboardStyles.emptyGroupContainer}><h2>No channels linked to this group. Use or share the invite link to start adding more channels</h2></section>}
             <br></br>
             <button onClick={() => 
                 popup("Delete the group", "This action is irreversible. All channels linked to your group will be unlinked.", "error", { icon: meteor, close: true, customButtonName: "Delete", action: function() {
