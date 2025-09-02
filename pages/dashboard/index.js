bc05fb90630b326140121a222ffdc8fe32bd80eb
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-09-02 18:38:24 +0200

    clean

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -308,15 +308,6 @@ export default function Dashboard() {
       </nav>
       <div className={styles.page}>
         <h1 className={styles.title}>{guild.name}</h1>
-        {/*<a href='/docs' target="_blank" rel="noreferrer">
-                <button className={styles.button}>
-                    Documentation <strong><svg fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                    <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                    <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
-                    </svg></strong>
-                </button>
-        </a>*/}
         <a
           href="https://discord.gg/PJumX8FjRV"
           target="_blank"
