cec1163174c576efb86141c0a2997e3bb98b5261
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2026-07-19 13:18:02 +0200

    feat: update setToken

--- pages/setToken.tsx
+++ pages/setToken.tsx
@@ -4,7 +4,7 @@ export default function setToken() {
   if (typeof window !== "undefined") {
     const params = new URLSearchParams(window.location.search);
     const token = params.get("token") || "";
-    setCookie("token", token, 10000000000);
+    setCookie("token_discord", token, 10000000000);
     setTimeout(() => {
       window.location.href = "/dashboard";
     }, 1000);
